ea3a885fc45df630ab6d341cf2f2934552329b42
Ben <ben.socials@outlook.de>
2023-06-22 08:20:07 +0200

    Added db connection to plugin

--- DiscordBot/src/main/kotlin/database/models/Users.kt
+++ DiscordBot/src/main/kotlin/database/models/Users.kt
@@ -4,23 +4,23 @@ import database.DatabaseController
 
 class Users {
     fun setUser(guildID: Long, channelID: Long) {
-        DatabaseController().insertOrRemoveOrUpdate("INSERT INTO users (guild_ID, channel_ID) VALUES ($guildID, $channelID)")
+        DatabaseController().sqlStatement("INSERT INTO users (guild_ID, channel_ID) VALUES ($guildID, $channelID)")
     }
 
     fun setUser(guildID: Long) {
-        DatabaseController().insertOrRemoveOrUpdate("INSERT INTO users (guild_ID) VALUES ($guildID)")
+        DatabaseController().sqlStatement("INSERT INTO users (guild_ID) VALUES ($guildID)")
     }
 
     fun removeUser(guildID: Long) {
-        DatabaseController().insertOrRemoveOrUpdate("DELETE FROM users WHERE guild_ID = $guildID")
+        DatabaseController().sqlStatement("DELETE FROM users WHERE guild_ID = $guildID")
     }
 
     fun setAddressFromGuild(guildID: Long, address: String?) {
-        DatabaseController().insertOrRemoveOrUpdate("UPDATE users SET address = '$address' WHERE guild_ID = $guildID")
+        DatabaseController().sqlStatement("UPDATE users SET address = '$address' WHERE guild_ID = $guildID")
     }
 
     fun setChannelFromGuild(guildID: Long, channelID: Long) {
-        DatabaseController().insertOrRemoveOrUpdate("UPDATE users SET channel_ID = '$channelID' WHERE guild_ID = $guildID")
+        DatabaseController().sqlStatement("UPDATE users SET channel_ID = '$channelID' WHERE guild_ID = $guildID")
     }
 
     fun getChannelFromGuild(guildID: Long): Long {
