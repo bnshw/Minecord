8441750e083d1f8900e868f6fa90362bc0483ac0
Ben <ben.socials@outlook.de>
2023-06-27 10:05:51 +0200

    Cleanup

--- DiscordBot/src/main/kotlin/database/models/Whitelist.kt
+++ DiscordBot/src/main/kotlin/database/models/Whitelist.kt
@@ -41,31 +41,4 @@ class Whitelist {
         query.close()
         return !uuidString.isNullOrEmpty()
     }
-
-
-    fun getUuidFromName(name: String): UUID? {
-        val query = DatabaseController().query("SELECT uuid FROM whitelist WHERE name = '$name'")
-
-        var uuid: UUID? = null
-
-        while (query.next()) {
-            uuid = UUID.fromString(query.getString("uuid"))
-        }
-
-        query.close()
-        return uuid
-    }
-
-    fun getGuildFromUUID(uuid: UUID): Long {
-        val query = DatabaseController().query("SELECT guild_ID FROM whitelist WHERE uuid = $uuid")
-
-        var guildID: Long = 0
-
-        while (query.next()) {
-            guildID = query.getLong("guild_ID")
-        }
-
-        query.close()
-        return guildID
-    }
 }
